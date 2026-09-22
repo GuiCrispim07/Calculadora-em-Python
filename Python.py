ece65769dd3df22d54e8537d94026c5d53303b31
Guilherme Crispim <guilhermecrispim107@gmail.com>
2026-09-22 12:25:25 -0300

    Refactor calculator prompts and output messages
    Updated prompts and output messages for clarity and corrected spelling.

--- Python.py
+++ Python.py
@@ -7,33 +7,32 @@ while True:
     a = int(input("Digite o primeiro valor: "))
     b = int(input("Digite o segundo valor: "))
 
-    op = int(input("Digite a operação desejada: 1 - Soma, 2 -Substracao, 3 - multiplicacao ou 4 - divisa0: "))
+    op = int(input("Digite a operacao desejada: 1 - soma, 2 -substracao, 3 - multiplicacao ou 4 - divisao: "))
     if op == 1:
         c = a + b
-        print("O resultado da soma é: ", c)
+        print("O resultado da soma e: ", c)
 
     elif op == 2:
         c = a - b
-        print("O resultado da subtração é: ", c)
+        print("O resultado da subtracao e: ", c)
 
     elif op == 3:
         c = a * b
-        print("O resultado da multiplicação é: ", c)
+        print("O resultado da multiplicacao e: ", c)
 
     elif op == 4:
         if b != 0:
             c = a / b
-            print("O resultado da divisão é: ", c)
+            print("O resultado da divisao e: ", c)
         else:
-            print("Não é possível dividir por zero.")
+            print("Nao e possível dividir por zero.")
 
     else:
-        print("Operação inválida.")
+        print("Operacao invalida.")
 
 
     if input("Deseja continuar? (s/n): ") != "s":
         break
-    
 
 
 
